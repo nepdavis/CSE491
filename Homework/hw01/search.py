37f4ea588f5ce71ec1e3a03c4ad209bc8ab71d09
nepdavis <ndavis2052@gmail.com>
2018-01-18 10:47:10 -0500

    Algorithm update with format -- needs to be tested but otherwise complete

---
+++ Homework/hw01/search.py
@@ -0,0 +1,26 @@
+t = "GTTTCTAACCTTATTACTACGTAACGAGGTCGTCATGCAATGCTGACCATGCATGCCTGACTCTGACGCTAACGTAACGGCAGCTGACGCTAGCCT"
+p = "ACGTAACG"
+
+n = len(t)
+m = len(p)
+
+total = 0
+
+positions = []
+
+for i in range(n - m + 1):
+
+    new = t[i:i+m]
+
+    j = 0
+
+    while (j < m) and (new[j] == p[j]):
+
+        j += 1
+
+    if j == m:
+
+        total += 1
+        positions.append(str(i))
+
+print("Total:", total, "", " ".join(positions))
