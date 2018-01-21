55cab30f2ce114a9e8edee56fc968e25316373ac
nepdavis <ndavis2052@gmail.com>
2018-01-20 20:07:07 -0500

    Putting naive pattern matching into method called naive() and created method called kmp(). This method implements the KMP algorithm. Not yet finished

--- Homework/hw01/search.py
+++ Homework/hw01/search.py
@@ -1,26 +1,55 @@
-t = ((open(input(""), "r")).readline()).strip()
-p = ((open(input(""), "r")).readline()).strip()
+def naive(t, p):
 
-n = len(t)
-m = len(p)
+    n = len(t)
+    m = len(p)
 
-total = 0
+    total = 0
 
-positions = []
+    positions = []
 
-for i in range(n - m + 1):
+    for i in range(n - m + 1):
 
-    new = t[i:i+m]
+        new = t[i:i+m]
 
-    j = 0
+        j = 0
 
-    while (j < m) and (new[j] == p[j]):
+        while (j < m) and (new[j] == p[j]):
 
-        j += 1
+            j += 1
 
-    if j == m:
+        if j == m:
 
-        total += 1
-        positions.append(str(i))
+            total += 1
+            positions.append(str(i))
 
-print("Total:", total, "", " ".join(positions))
+    print("Total:", total, "", " ".join(positions))
+
+
+def kmp(t, p):
+
+    n = len(t)
+    m = len(p)
+
+    total = 0
+
+    pi = [0 for _ in range(n)]
+
+    k = 0
+
+    for i in range(n):
+
+        while k >= 0 and p[k + 1] != t[i]:
+
+            k = pi[k]
+
+        k += 1
+
+        if k == m:
+
+            total += 1
+
+            k = pi[k]
+
+
+t_ = ((open(input(""), "r")).readline()).strip()
+p_ = ((open(input(""), "r")).readline()).strip()
