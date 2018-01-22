d707f421e802c74365fa9a70c2851ab222d4a62a
nepdavis <ndavis2052@gmail.com>
2018-01-21 20:46:15 -0500

    Fixed pi_array by incrementing k at the end of loop -- works for test case one. Added code to kmp() to keep track of positions where match is found and added it to returned result for printing

--- Homework/hw01/search.py
+++ Homework/hw01/search.py
@@ -29,17 +29,23 @@ def naive(t, p):
 
 def pi_array(pi, m, p):
 
-    pi[0] = 0
-
     k = 0
 
-    for i in range(m):
+    for i in range(1, m):
 
-        while k > 0 and p[k] != p[i]:
+        while k >= 0 and p[k + 1] != p[i]:
 
-            k = pi[k - 1]
+            if k == 0:
 
-        pi[i] = k + 1
+                pi[i] = 0
+
+            else:
+
+                k = pi[k - 1]
+
+        k += 1
+
+        pi[i] = k
 
 
 def kmp(t, p):
@@ -49,11 +55,17 @@ def kmp(t, p):
 
     total = 0
 
+    positions = []
+
     pi = [0 for _ in range(m)]
 
     k = 0
 
+    print(pi)
+
     pi_array(pi, m, p)
+
+    print(pi)
 
     for i in range(n):
 
@@ -67,11 +79,14 @@ def kmp(t, p):
 
             total += 1
 
-            k = pi[k]
+            positions.append(str(i))
+
+            k = pi[k - 1]
 
     pi = [str(i) for i in pi]
 
-    result = "Pi is: " + " ".join(pi) + "\n" + "Total: " + str(total)
+    result = "Pi is: " + " ".join(pi) + "\n" + "Total: " + str(total) +  \
+             "  " + " ".join(positions)
 
     return result
 
@@ -81,8 +96,8 @@ def main():
     t_ = ((open("T1.txt", "r")).readline()).strip()
     p_ = ((open("P1.txt", "r")).readline()).strip()
 
-    out = naive(t_, p_)
-    # out = kmp(t_, p_)
+    # out = naive(t_, p_)
+    out = kmp(t_, p_)
 
     print(out)
 
