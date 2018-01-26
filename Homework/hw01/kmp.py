f0a9c3ffaeddca18c2e8fec9ffd4887d11818e09
nepdavis <ndavis2052@gmail.com>
2018-01-26 13:50:28 -0500

    Removing naive() method and function call of naive() in main()

--- Homework/hw01/kmp.py
+++ Homework/hw01/kmp.py
@@ -1,35 +1,6 @@
 import time
 
 
-def naive(t, p):
-
-    n = len(t)
-    m = len(p)
-
-    total = 0
-
-    positions = []
-
-    for i in range(n - m + 1):
-
-        new = t[i:i+m]
-
-        j = 0
-
-        while (j < m) and (new[j] == p[j]):
-
-            j += 1
-
-        if j == m:
-
-            total += 1
-            positions.append(str(i))
-
-    result = "Total: " + str(total) + "  " + " ".join(positions)
-
-    return result
-
-
 def pi_array(pi, m, p):
 
     k = 0
@@ -106,10 +77,8 @@ def main():
     t_ = ((open("T3.txt", "r")).readline()).strip()
     p_ = ((open("P3.txt", "r")).readline()).strip()
 
-    out = naive(t_, p_)
     out_ = kmp(t_, p_)
 
-    print(out)
     print(out_)
 
     # # Gets file inputs (both on same line)
