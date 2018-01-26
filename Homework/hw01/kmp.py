50a0e7e482116a4e62dd1ca2aa50715c06202774
nepdavis <ndavis2052@gmail.com>
2018-01-26 13:48:41 -0500

    Fixed index position error -- kmp algorithm has consistent output with naive

--- Homework/hw01/kmp.py
+++ Homework/hw01/kmp.py
@@ -1,3 +1,6 @@
+import time
+
+
 def naive(t, p):
 
     n = len(t)
@@ -33,19 +36,22 @@ def pi_array(pi, m, p):
 
     for i in range(1, m):
 
-        while k >= 0 and p[k + 1] != p[i]:
+        while k >= 0 and i < m - 1 and p[k] != p[i]:
 
             if k == 0:
 
                 pi[i] = 0
+                break
 
             else:
 
                 k = pi[k - 1]
 
-        k += 1
+        if p[k] == p[i]:
 
-        pi[i] = k
+            k += 1
+
+            pi[i] = k
 
 
 def kmp(t, p):
@@ -83,9 +89,9 @@ def kmp(t, p):
 
             total += 1
 
-            k = pi[k - 1]
+            positions.append(str(i - k + 1))
 
-            positions.append(str(i - k))
+            k = pi[k - 1]
 
     pi = [str(i) for i in pi]
 
@@ -97,14 +103,32 @@ def kmp(t, p):
 
 def main():
 
-    t_ = ((open("T1.txt", "r")).readline()).strip()
-    p_ = ((open("P1.txt", "r")).readline()).strip()
+    t_ = ((open("T3.txt", "r")).readline()).strip()
+    p_ = ((open("P3.txt", "r")).readline()).strip()
 
     out = naive(t_, p_)
     out_ = kmp(t_, p_)
 
     print(out)
     print(out_)
+
+    # # Gets file inputs (both on same line)
+    # files = input().split()
+    #
+    # # Opens the text file and pattern file (text file comes first)
+    # t_ = (open(files[0]).readline()).strip()
+    # p_ = (open(files[1]).readline()).strip()
+    #
+    # prev_time = time.time()
+    #
+    # # calls kmp() method and stores in out
+    # out = kmp(t_, p_)
+    #
+    # new_time = time.time()
+    #
+    # # prints the kmp() return
+    # print(out)
+    # print("Total time elapsed:", round(new_time - prev_time, 10))
 
 if __name__ == "__main__":
 
