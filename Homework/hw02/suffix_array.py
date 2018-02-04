3402e29e1b09904d0701288fbafa856af2b11961
Nate Davis <ndavis2052@gmail.com>
2018-02-04 01:57:21 -0500

    Added binary_search() method

--- Homework/hw02/suffix_array.py
+++ Homework/hw02/suffix_array.py
@@ -1,6 +1,34 @@
 import time
 
 
+def binary_search(array, pattern):
+
+    left = 0
+    right = len(array)
+
+    while array[left] < pattern < array[right]:
+
+        c = (left + right) // 2
+
+        if pattern < array[c]:
+
+            if c == left + 1:
+
+                return c
+
+            right = c + 0
+            continue
+
+        elif pattern > array[c]:
+
+            if c == right - 1:
+
+                return c
+
+            left = c + 0
+            continue
+
+
 def suffix_array(text):
 
     array = [text[i:] for i in range(len(text))]
@@ -22,7 +50,8 @@ def main():
 
     s_array = suffix_array(text)
 
-    print("Suffix array build time was", time.time() - pre_build_time)
+    print("Suffix array build time was {:.6f} seconds".
+          format(time.time() - pre_build_time))
 
     print(s_array)
 
