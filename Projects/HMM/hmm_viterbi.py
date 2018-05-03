3ee6b0f1344af57cd32a3acbcc5b177e5a623872
Nate Davis <ndavis2052@gmail.com>
2018-05-02 21:42:51 -0400

    Added comments and doc to hmm_viterbi

--- Projects/HMM/hmm_viterbi.py
+++ Projects/HMM/hmm_viterbi.py
@@ -4,6 +4,12 @@ import numpy as np
 
 def parse_file(filename):
 
+    """
+    Function to read in .fa file
+    :param filename: .fa file
+    :return: data as one string
+    """
+
     data = ""
 
     with open(filename) as f:
@@ -20,6 +26,12 @@ def parse_file(filename):
 
 
 def read_params(filename):
+
+    """
+    Function to read in example.hmm parameters
+    :param filename: example.hmm
+    :return: parameters
+    """
 
     lines = []
 
@@ -43,109 +55,125 @@ def read_params(filename):
 
     # Parse third line
 
-    prob_a_a, prob_a_b = [float(i) for i in lines[2].split()[:2]]
+    prob_table_a = {emissions[i]: float(lines[2].split()[2:][i]) for i in range(4)}
 
-    prob_emission_a = {emissions[i]: float(lines[2].split()[2:][i]) for i in range(4)}
+    prob_table_a["AA"], prob_table_a["AB"] = [float(i) for i in lines[2].split()[:2]]
 
     # Parse fourth line
 
-    prob_b_a, prob_b_b = [float(i) for i in lines[3].split()[:2]]
+    prob_table_b = {emissions[i]: float(lines[3].split()[2:][i]) for i in range(4)}
 
-    prob_emission_b = {emissions[i]: float(lines[3].split()[2:][i]) for i in range(4)}
+    prob_table_b["BA"], prob_table_b["BB"] = [float(i) for i in lines[3].split()[:2]]
 
-    return prob_a, prob_b, prob_a_a, prob_a_b, prob_b_a, \
-        prob_b_b, prob_emission_a, prob_emission_b
+    return prob_a, prob_b, prob_table_a, prob_table_b
 
 
-def viterbi(data, params):
+def viterbi(data, prob_a, prob_b, a_probs, b_probs):
 
-    # Transform probabilities to log space
-    prob_a, prob_b, prob_a_a, prob_a_b, prob_b_a, \
-    prob_b_b = [np.log2(i) for i in params[:6]]
+    """
+    HMM and Viterbi algorithm for state transitions
+    :param data: .fa as a string
+    :param prob_a: init prob for a
+    :param prob_b: init prob for b
+    :param a_probs: a parameters
+    :param b_probs: b parameters
+    :return: state transitions
+    """
 
-    emissions_a = {k: np.log2(v) for k, v in params[6].items()}
-    emissions_b = {k: np.log2(v) for k, v in params[7].items()}
+    # transform to log space
+    a_probs = {k: np.log(v) for k, v in a_probs.items()}
+    b_probs = {k: np.log(v) for k, v in b_probs.items()}
 
-    # Initialize viterbi tables
-    viterbi_a = [prob_a + emissions_a[data[0]]]
-    viterbi_b = [prob_b + emissions_b[data[0]]]
+    # init viterbi
+    viterbi_a = [np.log(prob_a) + a_probs[data[0]]]
+    viterbi_b = [np.log(prob_b) + b_probs[data[0]]]
 
-    # Run viterbi algorithm
+    # create array for path state transitions
+    path_states = np.zeros((2, len(data)), dtype = "int")
+
+    # init path states
+    path_states[1, 0] = 1
+
+    # for each value in the .fa file
     for i in range(1, len(data)):
 
-        a = max(emissions_a[data[i]] + viterbi_a[i-1] + prob_a_a,
-                emissions_a[data[i]] + viterbi_b[i-1] + prob_b_a)
+        a_options = [viterbi_a[i-1] + a_probs["AA"],
+                     viterbi_b[i-1] + b_probs["BA"]]
 
-        b = max(emissions_b[data[i]] + viterbi_a[i-1] + prob_a_b,
-                emissions_b[data[i]] + viterbi_b[i-1] + prob_b_b)
+        # max probability of a
+        a = a_probs[data[i]] + max(a_options)
+
+        # get path for new a
+        path_states[0, i] = a_options.index(max(a_options))
+
+        b_options = [viterbi_a[i-1] + a_probs["AB"],
+                     viterbi_b[i-1] + b_probs["BB"]]
+
+        # max probability for b
+        b = b_probs[data[i]] + max(b_options)
+
+        # get path for new b
+        path_states[1, i] = b_options.index(max(b_options))
 
         viterbi_a.append(a)
         viterbi_b.append(b)
 
-    print(viterbi_a[718:723])
-    print(viterbi_b[718:723])
+    # get state with max probability at end
+    max_index = 0 if viterbi_a[-1] > viterbi_b[-1] else 1
 
-    return viterbi_a, viterbi_b
+    # initialize path
+    path = [max_index]
+
+    i = path[0]
+
+    # tracing back, find optimal state for value given where it tranfered from
+    for j in range(len(data)-2, -1, -1):
+
+        i = path_states[i, j]
+
+        path.append(i)
+
+    # transform path to A and B (state names)
+    path = ["A" if i == 0 else "B" for i in path]
+
+    # return reversed path (correct order)
+    return path[::-1]
 
 
-def traceback(viterbi_a, viterbi_b):
+def output(path, output_file):
 
-    states = {"A": "B", "B": "A"}
+    """
+    Function to write algorithm output to file
+    :param path: paths
+    :param output_file: file name to write to
+    :return: none
+    """
 
     lines = []
-
+    states = {"A": 0, "B": 0}
     current_line = "{:>10}".format(1)
 
-    state_numbers = {"A": 0, "B": 0}
+    for i in range(1, len(path)):
 
-    if viterbi_a[0] > viterbi_b[0]:
+        if path[i - 1] != path[i]:
 
-        current_state = "A"
+            current_line += "{:>10}".format(i-1) + " state " + path[i-1]
 
-    else:
+            lines.append(current_line)
 
-        current_state = "B"
+            current_line = "{:>10}".format(i)
 
-    for i in range(1, len(viterbi_a)):
+            states[path[i-1]] += 1
 
-        if viterbi_a[i] > viterbi_b[i]:
+    current_line += "{:>10}".format(len(path)) + " state " + path[i - 1]
 
-            if current_state != "A":
-
-                current_line += "{:>10}".format(i) + " state " + current_state
-
-                lines.append(current_line)
-
-                current_line = "{:>10}".format(i + 1)
-
-                state_numbers[current_state] += 1
-
-                current_state = states[current_state]
-
-        #elif viterbi_b[i] > viterbi_a[i] and current_state != "B":
-        else:
-
-            if current_state != "B":
-
-                current_line += "{:>10}".format(i) + " state " + current_state
-
-                lines.append(current_line)
-
-                current_line = "{:>10}".format(i + 1)
-
-                state_numbers[current_state] += 1
-
-                current_state = states[current_state]
-
-    current_line += "{:>10}".format(i + 1) + " state " + current_state
     lines.append(current_line)
 
-    return lines, state_numbers
+    states[path[i-1]] += 1
 
+    file_name = "problem_2_output_" + str(output_file) + ".txt"
 
-def output(lines, states):
-
-    text_file = open("problem_2_output.txt", "w")
+    text_file = open(file_name, "w")
 
     first_line = "State A: {}, State B: {}\n".format(states["A"], states["B"])
 
@@ -164,13 +192,13 @@ def main():
 
     data = parse_file(file)
 
-    params = read_params(params_file)
+    a_prob, b_prop, all_a, all_b = read_params(params_file)
 
-    hmm_a, hmm_b = viterbi(data, params)
+    path = viterbi(data, a_prob, b_prop, all_a, all_b)
 
-    output_lines, states = traceback(hmm_a, hmm_b)
+    new_file = file.replace("/", "_").replace(".", "_").strip()
 
-    output(output_lines, states)
+    output(path, new_file)
 
 
 if __name__ == "__main__":
