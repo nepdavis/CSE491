efb96e351061bfbb9827687d36454153fe3c56b4
Nate Davis <ndavis2052@gmail.com>
2018-05-02 21:13:55 -0400

    Changes

--- Projects/EM/problem_1_A.py
+++ Projects/EM/problem_1_A.py
@@ -56,11 +56,11 @@ def mle(first_rolls, second_rolls, dice_rolls):
 
     for roll in range(1, 7):
 
-        first_value = first_rolls.count(roll) / len(first_rolls)
+        first_value = first_rolls.count(roll)# / len(first_rolls)
 
         first_probs.append(first_value)
 
-        second_value = second_rolls.count(roll) / len(second_rolls)
+        second_value = second_rolls.count(roll)# / len(second_rolls)
 
         second_probs.append(second_value)
 
